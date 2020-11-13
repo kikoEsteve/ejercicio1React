45bb0eee6e80685cf4bb0e11ee63d86cfcb01947
kikoEsteve <72131371+kikoEsteve@users.noreply.github.com>
2020-11-13 10:47:19 +0100

    myfirstcommit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+const Person = props => {
+  console.log(props)
+  return (<div>
+    <span>Nombre: {props.name}, </span>
+    <span>Apellido: {props.name}, </span>
+    <span>Edad: {props.age}.</span>
+  </div>)
+}
+
+function App() {
+  const hello = <h2>Hello World</h2>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Person name="Kiko" surname="Esteve" age={39} />
+        <Person name="Antonio" surname="Nose" age={35} />
+        <Person name="Pedro" surname="Picapiedra" age={20000} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
